df62f7c8c7abf4c4cdbd294e9626a6b90b030467
Kyle Learned <kyle.learned.this@gmail.com>
2019-03-03 22:46:13 -0800

    BaseSort added

---
+++ SwiftDS/sort/BubbleSort.swift
@@ -0,0 +1,14 @@
+//
+//  BubbleSort.swift
+//  SwiftDS
+//
+//  Copyright © 2019 kylelearnedthis. All rights reserved.
+//
+
+import Foundation
+
+public class BubbleSort: BaseSort, Sorter {
+    func sort() {
+        self.print()
+    }
+}
---
+++ SwiftDSTests/sort/BubbleSortTest.swift
@@ -0,0 +1,18 @@
+//
+//  BubbleSortTest.swift
+//  SwiftDSTests
+//
+//  Copyright © 2019 kylelearnedthis. All rights reserved.
+//
+
+import XCTest
+
+class BubbleSortTest: XCTestCase {
+
+    func testBasic() {
+        let n: [Int] = [9,8,7,6,5]
+        let x = BubbleSort(input: n)
+        x.sort()
+    }
+
+}
