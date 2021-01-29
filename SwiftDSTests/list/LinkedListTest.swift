1dd6e4d8c8a8954851770d099f4cac4b98187d65
Kyle Learned <kyle.learned.this@gmail.com>
2021-01-29 00:13:47 -0800

    LinkedList: added Prepend, and Delete operations

--- SwiftDSTests/list/LinkedListTest.swift
+++ SwiftDSTests/list/LinkedListTest.swift
@@ -9,7 +9,19 @@ import XCTest
 
 class LinkedListTest: XCTestCase {
 
-    func testAdd() {
+    func testPrepend() {
+        let input = [1,2,3,4,5,6,7]
+        let list = LinkedList<Int>()
+        for i in input {
+            list.prepend(value: i)
+        }
+        print("===== print Standard =====")
+        list.printList()
+        print("===== print Reverse =====")
+        list.printReverseList()
+    }
+
+    func testAppend() {
         let input = [1,2,3,4,5,6,7]
         let list = LinkedList<Int>(input: input)
         print("===== print Standard =====")
@@ -28,4 +40,28 @@ class LinkedListTest: XCTestCase {
         }
     }
 
+    func testDeleteBasic() {
+        let input = [1,2,3,4,5,6,7]
+        testDeleteWork(input: input, value: 2)
+    }
+
+    func testDeleteAtTail() {
+        let input = [1,2,3,4,5,6,7]
+        testDeleteWork(input: input, value: 7)
+    }
+
+    func testDeleteAtHead() {
+        let input = [1,2,3,4,5,6,7]
+        testDeleteWork(input: input, value: 1)
+    }
+
+    private func testDeleteWork(input: [Int], value: Int) {
+        let list = LinkedList<Int>(input: input)
+        print("===== print =====")
+        list.printList()
+        let deleted = list.delete(value: value)
+        XCTAssertTrue(deleted, "Failed to delete")
+        print("===== print after delete =====")
+        list.printList()
+    }
 }
