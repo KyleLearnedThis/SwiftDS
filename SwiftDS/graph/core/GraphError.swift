b5fe4e49a22278f74130dce1e9377f37d3c40df5
KyleLearnedThis <kyle.learned.this@gmail.com>
2021-02-12 03:21:08 -0800

    Refactor Graph, implement BFS solution and provide unit test (#22)

---
+++ SwiftDS/graph/core/GraphError.swift
@@ -0,0 +1,14 @@
+//
+//  GraphError.swift
+//  SwiftDS
+//
+//  Created by KyleLearnedThis on 2/12/21.
+//  Copyright © 2021 kylelearnedthis. All rights reserved.
+//
+
+import Foundation
+
+enum GraphError: Error {
+    case noRootNodeFound
+    case noNodeFound
+}
---
+++ SwiftDSTests/graph/core/GraphTest.swift
@@ -0,0 +1,24 @@
+//
+//  GraphTest.swift
+//  SwiftDSTests
+//
+//  Created by KyleLearnedThis on 2/12/21.
+//  Copyright © 2021 kylelearnedthis. All rights reserved.
+//
+
+import XCTest
+
+class GraphTest: XCTestCase {
+
+    func testBasic() throws {
+        let dir = "dev/iOS/SwiftDS/SwiftDSTests/graph/resources/"
+        let fileName = "test01.json"
+        let graph = Graph(docFileDir: dir, fileName: fileName)
+        let starCity = "San Francisco"
+        let searchCity = "Miami"
+        let expected = true
+        let actual = try graph.bfsSearch(root: starCity, key: searchCity)
+        XCTAssertEqual(actual, expected, "Expected to find \(searchCity) but is not found")
+    }
+
+}
